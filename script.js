e9db4b3922c89edcaedc8b715f733d3a6235d81e
verycoldyiting <S10262676@connect.np.edu.sg>
2023-12-01 21:59:33 +0800

    FED assignment

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+script.js
+
 
 document.addEventListener('DOMContentLoaded', function () {
   // Show the pop-up after a delay
@@ -32,4 +34,91 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+function validateForm() {
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value.trim();
 
+  if (name == "") {
+    alert("Name can't be blank");
+    return false;
+  }
+
+  // Capitalize the name
+  var capitalizedName = capitalizeFirstLetter(name);
+
+  // Assign the capitalized name back to the input field
+  nameInput.value = capitalizedName;
+
+  // Validate email
+  var emailInput = document.getElementById('email');
+  var email = emailInput.value.trim();
+
+  if (!validateEmail(email)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+
+  // Validate phone number
+  var phoneInput = document.getElementById('phone');
+  var phone = phoneInput.value.trim();
+
+  if (!validatePhone(phone)) {
+    alert("Please enter a valid phone number");
+    return false;
+  }
+
+  // Additional validation for other fields can be added here if needed.
+
+  // If all validations pass, show the success popup
+  showSuccessPopup();
+
+  // Prevent the default form submission
+  return false;
+}
+
+function validateEmail(email) {
+  // Create a regular expression for basic email validation
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Test if the provided email matches the regular expression
+  var isValidEmail = regex.test(email);
+
+  // Return the result of the test (true if valid, false otherwise)
+  return isValidEmail;
+}
+
+
+function validatePhone(phone) {
+  // Remove any non-digit characters
+  var digitsOnly = phone.replace(/\D/g, "");
+
+  // Check if the cleaned phone number has at least 8 digits
+  if (digitsOnly.length >= 8) {
+    return true; // Valid phone number
+  } else {
+    return false; // Invalid phone number
+  }
+}
+
+function capitalizeFirstLetter(str) {
+  return str.replace(/\b\w/g, function (match) {
+    return match.toUpperCase();
+  });
+}
+
+function showSuccessPopup() {
+  var popupContainer = document.querySelector('.popupcontainer');
+  popupContainer.style.display = 'block';
+
+  // Close the popup when the "OK" button is clicked
+  var okBtn = document.getElementById('okBtn');
+  okBtn.addEventListener('click', function () {
+    popupContainer.style.display = 'none';
+  });
+
+  // Close the popup when the close button is clicked
+  var closeBtn = document.querySelector('.closebtn');
+  closeBtn.addEventListener('click', function () {
+    popupContainer.style.display = 'none';
+  });
+}
